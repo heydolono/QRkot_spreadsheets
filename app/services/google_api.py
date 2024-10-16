7769886595e0b16c999f32346a9108dca7cf4b2a
heydolono <makskol43@gmail.com>
2024-10-16 18:20:45 +0300

    fix bugs

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -10,21 +10,21 @@ SPREADSHEET_BODY = {
     'properties': {'title': f'Отчёт на {NOW_DATE_TIME}',
                             'locale': 'ru_RU'},
     'sheets': [{'properties': {'sheetType': 'GRID',
-                'sheetId': 0, 'title': 'Лист1',
-                'gridProperties': {'rowCount': 100,
-                                            'columnCount': 11}}}]
+                'sheetId': 0, 'title': 'Лист1', 'gridProperties': {
+                    'rowCount': 100,
+                    'columnCount': 11}}}]
 }
 PERMISSIONS_BODY = {'type': 'user',
                     'role': 'writer',
                     'emailAddress': settings.email}
 TABLE_VALUES = [
-        ['Отчёт от', NOW_DATE_TIME],
-        ['Топ проектов по скорости закрытия'],
-        ['Название проекта', 'Время сбора', 'Описание']
+    ['Отчёт от', NOW_DATE_TIME],
+    ['Топ проектов по скорости закрытия'],
+    ['Название проекта', 'Время сбора', 'Описание']
 ]
 UPDATE_BODY = {
-        'majorDimension': 'ROWS',
-        'values': TABLE_VALUES
+    'majorDimension': 'ROWS',
+    'values': TABLE_VALUES
 }
 
 
