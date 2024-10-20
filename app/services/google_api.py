15c06f962a2d1c8d619171ffdbf11160327ebfdb
heydolono <makskol43@gmail.com>
2024-10-20 21:52:15 +0300

    fix bugs

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -5,20 +5,19 @@ from aiogoogle import Aiogoogle
 from app.core.config import settings
 
 FORMAT = "%Y/%m/%d %H:%M:%S"
-NOW_DATE_TIME = datetime.now().strftime(FORMAT)
 SPREADSHEET_BODY = {
-    'properties': {'title': f'Отчёт на {NOW_DATE_TIME}',
-                            'locale': 'ru_RU'},
+    'properties': {'title': 'Отчёт на временное значение', 'locale': 'ru_RU'},
     'sheets': [{'properties': {'sheetType': 'GRID',
-                'sheetId': 0, 'title': 'Лист1', 'gridProperties': {
-                    'rowCount': 100,
-                    'columnCount': 11}}}]
+                               'sheetId': 0,
+                               'title': 'Лист1',
+                               'gridProperties': {'rowCount': 100,
+                                                  'columnCount': 11}}}]
 }
 PERMISSIONS_BODY = {'type': 'user',
                     'role': 'writer',
                     'emailAddress': settings.email}
 TABLE_VALUES = [
-    ['Отчёт от', NOW_DATE_TIME],
+    ['Отчёт от', 'временное значение'],
     ['Топ проектов по скорости закрытия'],
     ['Название проекта', 'Время сбора', 'Описание']
 ]
@@ -29,6 +28,9 @@ UPDATE_BODY = {
 
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
+    now_date_time = datetime.now().strftime(FORMAT)
+    spreadsheet_body = SPREADSHEET_BODY.copy()
+    spreadsheet_body['properties']['title'] = f'Отчёт на {now_date_time}'
     service = await wrapper_services.discover('sheets', 'v4')
     response = await wrapper_services.as_service_account(
         service.spreadsheets.create(json=SPREADSHEET_BODY)
@@ -55,6 +57,9 @@ async def spreadsheets_update_value(
         projects: list,
         wrapper_services: Aiogoogle
 ) -> None:
+    now_date_time = datetime.now().strftime(FORMAT)
+    table_values = TABLE_VALUES.copy()
+    table_values[0][1] = now_date_time
     service = await wrapper_services.discover('sheets', 'v4')
     for project in projects:
         collection_time = project.close_date - project.create_date
